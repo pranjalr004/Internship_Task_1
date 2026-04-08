4bb42fd3a07a8eec5e63f4a7579c96fae67a48dc
pranjalr004 <pranjalr376@gmail.com>
2026-04-08 13:41:31 +0530

    Student Table created successfully

---
+++ Database_Setup/Table_Creation.sql
@@ -0,0 +1,10 @@
+CREATE Table Students(
+    StudentID INT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(20),
+    Gender VARCHAR(10),
+    Age INT,
+    Grade varchar(5),
+    MathScore INT,
+    ScienceScore INT,
+    EnglishScore INT
+)
